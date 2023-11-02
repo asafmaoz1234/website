57df924c92025f18386aa41be9129aeb4159ddc9
asafmaoz1234 <asafmaoz27@gmail.com>
2023-11-02 15:35:25 +0200

    second run with populate table

--- projects/seavision/script.js
+++ projects/seavision/script.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const coastalAreaSelector = document.getElementById('coastal-area-selector');
     const resultsTable = document.getElementById('results-table');
     const registrationForm = document.getElementById('registration-form');
+    const resultsPlaceholder = document.getElementById('results-placeholder');
+
 
     // Object to store cached data for each coastal area
     const cachedData = {};
@@ -13,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
             return Promise.resolve(cachedData[coastalArea]);
         } else {
             // Replace with your server API URL
-            return fetch(`https://example.com/api/data?coastalArea=${coastalArea}`)
+            return fetch(`https://jsonplaceholder.typicode.com/posts?coastalArea=${coastalArea}`)
                 .then(response => response.json())
                 .then(data => {
                     cachedData[coastalArea] = data; // Cache the data
@@ -31,9 +33,9 @@ document.addEventListener('DOMContentLoaded', function () {
         // Create table rows with data
         data.forEach(entry => {
             const row = resultsTable.insertRow();
-            row.insertCell(0).textContent = entry.date;
-            row.insertCell(1).textContent = `Wave Height: ${entry.waveHeight} meters`;
-            row.insertCell(2).textContent = `Weather: ${entry.weather}`;
+            row.insertCell(0).textContent = entry.userId;
+            row.insertCell(1).textContent = `Wave Height: ${entry.id} meters`;
+            row.insertCell(2).textContent = `Weather: ${entry.title}`;
         });
     }
 
@@ -44,6 +46,20 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(data => {
                 populateTable(data);
             });
+    });
+
+    // Add an event listener to the select element
+    coastalAreaSelector.addEventListener('change', function() {
+        // Check if a coastal area is selected
+        if (coastalAreaSelector.value !== 'default') {
+            // If a coastal area is selected, show the results table and hide the placeholder
+            resultsTable.style.display = 'table'; // Display the table as a table
+            resultsPlaceholder.style.display = 'none'; // Hide the placeholder
+        } else {
+            // If the default option is selected, hide the results table and show the placeholder
+            resultsTable.style.display = 'none'; // Hide the table
+            resultsPlaceholder.style.display = 'block'; // Display the placeholder
+        }
     });
 
     // Event listener for form submission
